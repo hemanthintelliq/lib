eed8c81a1ed51ed1cd3b5cfcffdbb55b955dfd5d
hemanthintelliq <125686164+hemanthintelliq@users.noreply.github.com>
2023-02-23 00:39:19 +0530

    Create CICD.groovy

---
+++ vars/CICD.groovy
@@ -0,0 +1,19 @@
+def newGit(repo)
+{
+   git "https://github.com/intelliqittrainings/${repo}.git"
+}
+
+def newMaven()
+{
+    sh 'mvn package'
+}
+
+def newdeploy("jobname","ip","contextpath")
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/{contextpath}.war"
+}
+
+def runselenium(jobname)
+{
+     sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
