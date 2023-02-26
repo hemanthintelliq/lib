0ac128e7c5312b76c6eb1d0bddda05699a4b2183
hemanthintelliq <125686164+hemanthintelliq@users.noreply.github.com>
2023-02-26 16:15:06 +0530

    Update ram.groovy

--- vars/ram.groovy
+++ vars/ram.groovy
@@ -6,3 +6,7 @@ def newMaven()
 {
     sh 'mvn package'
 }
+def newdeploy(jobname,ip,contextpath)
+{
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
+}
